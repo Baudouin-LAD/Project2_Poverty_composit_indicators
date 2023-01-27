3e76bd0b3a145b0c4c4cfac750a76971d706edd7
Kevin <kevin.thai@edhec.com>
2023-01-27 15:31:25 +0100

    add education

---
+++ kevin_sql_project2.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS Project2;
+USE Project2;
+
+CREATE TABLE IF NOT EXISTS sample_countries (
+	country_id varchar(255),
+	country_name varchar(255)
+);
+
+SELECT * FROM sample_countries;
